99116fb16494fb57e0d2de736e48710128565966
weikeniu <new@weikeniu.com>
2017-11-10 14:26:20 +0800

    小程序签到、节目打分

---
+++ pages/wei-scene/score-over/score-over.js
@@ -0,0 +1,18 @@
+// pages/wei-scene/score-over/score-over.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  }
+
+})
