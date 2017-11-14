a1b68a51e06c87eccba85313a27cc2bb2dcaac84
weikeniu <new@weikeniu.com>
2017-11-14 11:11:56 +0800

    签到页面修改

--- pages/wei-scene/sign/sign.js
+++ pages/wei-scene/sign/sign.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    array: ['设计推广部', '人事行政部', '运营部', '产品部', '客服部', '结算部', '微可牛运营部', '酒店用品采购部', '酒店合作部', '信用住', '飞猪旅行部', 'OTA客服部'],
+    index: 0
   },
 
   /**
@@ -13,6 +14,14 @@ Page({
    */
   onLoad: function (options) {
 
+  },
+
+  //部门选择
+  departmentSelect: function(e){
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
   }
 
 })
